d52917dd768ab281d17d5e14a840eb54da5f824e
Ramon <kudgar@gmail.com>
2021-09-21 18:43:56 +0300

    remove

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin (activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
+  slides[activeSlide].classList.add('active')
 
-    slide.classList.add("active");
-  });
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+  
+      slide.classList.add('active');
+    });
+  }
+  
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-}
+
+slidesPlugin();
